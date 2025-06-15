f3d248ed5089df777d22f26f38364ae65ab6522c
Vimal KR <vimalkr141@gmail.com>
2025-06-15 20:42:06 +0530

    about page update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
