cb5e4e6ebdf62008d91c32334cc022a6cf45d7e9
Vimal Kr <96064908+vimalkrml@users.noreply.github.com>
2025-06-15 20:49:35 +0530

    Update AboutUs.jsx

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import NavBar from "../Components/NavBar/NavBar";
-import { Container, Row, Col, Card, Badge, Image } from 'react-bootstrap';
+import { Container, Row, Col, Card, Badge } from 'react-bootstrap';
 import Footer from "../Components/Footer/Footer";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
